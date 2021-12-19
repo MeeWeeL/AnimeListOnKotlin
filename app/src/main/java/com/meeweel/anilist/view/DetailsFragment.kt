79e90e29a2e871abaac5126233fa6b7e36fb4eb8
MeeWeeL <MeeWeeL@yandex.ru>
2021-12-20 01:00:35 +0300

    Details fragment design

--- app/src/main/java/com/meeweel/anilist/view/DetailsFragment.kt
+++ app/src/main/java/com/meeweel/anilist/view/DetailsFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.meeweel.anilist.R
 import com.meeweel.anilist.databinding.DetailsFragmentBinding
 import com.meeweel.anilist.model.data.Anime
+import com.meeweel.anilist.model.room.isRussian
 import com.meeweel.anilist.viewmodel.ImageMaker
 
 class DetailsFragment : Fragment() {
@@ -34,16 +36,19 @@ class DetailsFragment : Fragment() {
     @SuppressLint("SetTextI18n")
     private fun populateData(animeData: Anime) {
         with(binding) {
+            binding.detailsBarImage.setImageBitmap(imageMaker.getPictureFromDirectory(animeData.image))
+            binding.detailsDescriptionImage.setImageBitmap(imageMaker.getPictureFromDirectory(animeData.image))
             animeImage.setImageBitmap(imageMaker.getPictureFromDirectory(animeData.image))
             originalTitle.text = animeData.originalTitle
             englishTitle.text = animeData.enTitle
             russianTitle.text = animeData.ruTitle
+            russianTitle.visibility = if (isRussian) View.VISIBLE else View.GONE
             descriptionValue.text = animeData.description
-            releaseAuthor.text = animeData.author
-            releaseGenre.text = animeData.genre
-            releaseData.text = animeData.data
-            releaseAgeRate.text = animeData.ageRating.toString() + "+"
-            releaseRating.text = animeData.rating.toString() + "%"
+            releaseAuthor.text = "${getText(R.string.author)}: ${animeData.author}"
+            releaseGenre.text = "${getText(R.string.genre)}: ${animeData.genre}"
+            releaseData.text = "${getText(R.string.data)}: ${animeData.data}"
+            releaseAgeRate.text = "${getText(R.string.age_rating)}: ${animeData.ageRating} +"
+            releaseRating.text = "${getText(R.string.rating)}: ${animeData.rating} %"
         }
     }
 
