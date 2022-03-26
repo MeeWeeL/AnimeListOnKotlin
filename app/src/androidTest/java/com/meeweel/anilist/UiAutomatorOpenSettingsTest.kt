2ef377d90546f08473b617f33fbfccba6c5a726b
MeeWeeL <MeeWeeL@yandex.ru>
2022-03-26 23:23:44 +0300

    Record Espresso Test

--- app/src/androidTest/java/com/meeweel/anilist/UiAutomatorOpenSettingsTest.kt
+++ app/src/androidTest/java/com/meeweel/anilist/UiAutomatorOpenSettingsTest.kt
@@ -1,5 +1,6 @@
 package com.meeweel.anilist
 
+import STEPS
 import android.widget.TextView
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
@@ -15,13 +16,13 @@ class UiAutomatorOpenSettingsTest {
     @Test
     fun test_OpenSettings() {
         uiDevice.pressHome()
-        uiDevice.swipe(500,600,500,100,10)
+        uiDevice.swipe(500,600,500,100, STEPS)
         val appViews = UiScrollable(UiSelector().scrollable(false))
         val className = UiSelector().className(TextView::class.java.name)
         val settingsApp = appViews.getChildByText(className, "Settings")
         settingsApp.clickAndWaitForNewWindow()
         val settingsValidation = uiDevice.findObject(UiSelector().packageName("com.android.settings"))
         Assert.assertTrue(settingsValidation.exists())
-        uiDevice.swipe(500,600,500,100,10)
+        uiDevice.swipe(500,600,500,100, STEPS)
     }
 }
