826b7f2fb9061eb350e38e5dd7091a36f62121b1
MeeWeeL <MeeWeeL@yandex.ru>
2022-09-14 22:52:42 +0300

    RELEASE 2.2

--- app/src/androidTest/java/com/meeweel/anilist/UiAutomatorOpenSettingsTest.kt
+++ app/src/androidTest/java/com/meeweel/anilist/UiAutomatorOpenSettingsTest.kt
@@ -1,6 +1,5 @@
 package com.meeweel.anilist
 
-import STEPS
 import android.widget.TextView
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
@@ -17,7 +16,7 @@ class UiAutomatorOpenSettingsTest {
     @Test
     fun test_OpenSettings() {
         uiDevice.pressHome()
-        uiDevice.swipe(500, 600, 500, 100, STEPS)
+        uiDevice.swipe(500, 600, 500, 100, 5)
         val appViews = UiScrollable(UiSelector().scrollable(false))
         val className = UiSelector().className(TextView::class.java.name)
         val settingsApp = appViews.getChildByText(className, "Settings")
@@ -25,6 +24,6 @@ class UiAutomatorOpenSettingsTest {
         val settingsValidation =
             uiDevice.findObject(UiSelector().packageName("com.android.settings"))
         Assert.assertTrue(settingsValidation.exists())
-        uiDevice.swipe(500, 600, 500, 100, STEPS)
+        uiDevice.swipe(500, 600, 500, 100, 5)
     }
 }
