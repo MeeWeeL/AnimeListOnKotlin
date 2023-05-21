7d0a972bd9472f90e3bf1863812525f5609b61e2
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-05-21 14:29:00 +0300

    Implement the use of the correct ViewHolder

--- app/src/main/java/com/meeweel/anilist/newPresentation/mainFragment/adapter/NewAnimeListAdapter.kt
+++ app/src/main/java/com/meeweel/anilist/newPresentation/mainFragment/adapter/NewAnimeListAdapter.kt
@@ -6,24 +6,65 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.ListAdapter
 import com.meeweel.anilist.databinding.MainRecyclerItemBinding
+import com.meeweel.anilist.databinding.NotWatchedRecyclerItemBinding
+import com.meeweel.anilist.databinding.UnwantedRecyclerItemBinding
+import com.meeweel.anilist.databinding.WantedRecyclerItemBinding
+import com.meeweel.anilist.databinding.WatchedRecyclerItemBinding
 import com.meeweel.anilist.domain.models.ShortAnime
+import com.meeweel.anilist.newPresentation.mainFragment.ListState
+import com.meeweel.anilist.newPresentation.mainFragment.adapter.viewHolders.BaseViewHolder
 import com.meeweel.anilist.newPresentation.mainFragment.adapter.viewHolders.NewMainViewHolder
+import com.meeweel.anilist.newPresentation.mainFragment.adapter.viewHolders.NotWatchedViewHolder
+import com.meeweel.anilist.newPresentation.mainFragment.adapter.viewHolders.UnwantedViewHolder
+import com.meeweel.anilist.newPresentation.mainFragment.adapter.viewHolders.WantedViewHolder
+import com.meeweel.anilist.newPresentation.mainFragment.adapter.viewHolders.WatchedViewHolder
 
-class NewAnimeListAdapter: ListAdapter<ShortAnime, NewMainViewHolder>(DiffCallback) {
+class NewAnimeListAdapter : ListAdapter<ShortAnime, BaseViewHolder>(DiffCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewMainViewHolder {
-        val binding = MainRecyclerItemBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
-        )
-        return NewMainViewHolder(binding)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return when (viewType) {
+            ListState.MAIN.int - 1 -> NewMainViewHolder(
+                MainRecyclerItemBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+            )
+
+            ListState.UNWANTED.int - 1 -> UnwantedViewHolder(
+                UnwantedRecyclerItemBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+            )
+
+            ListState.WANTED.int - 1 -> WantedViewHolder(
+                WantedRecyclerItemBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+            )
+
+            ListState.NOT_WATCHED.int - 1 -> NotWatchedViewHolder(
+                NotWatchedRecyclerItemBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+            )
+
+            ListState.WATCHED.int - 1 -> WatchedViewHolder(
+                WatchedRecyclerItemBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+            )
+
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
-    override fun onBindViewHolder(holder: NewMainViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return currentList[position].list - 1
+    }
     fun onItemSwipe(viewHolderPosition: Int, i: Int) {
         if (i == ItemTouchHelper.START) {
             // TODO
