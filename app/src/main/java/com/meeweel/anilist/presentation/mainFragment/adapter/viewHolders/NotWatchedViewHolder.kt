a1bb68fb9744263ad0f444eeb6f231c2f01826ad
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-06-13 19:34:18 +0300

    Added loading animation in MainFragment

--- app/src/main/java/com/meeweel/anilist/presentation/mainFragment/adapter/viewHolders/NotWatchedViewHolder.kt
+++ app/src/main/java/com/meeweel/anilist/presentation/mainFragment/adapter/viewHolders/NotWatchedViewHolder.kt
@@ -2,6 +2,7 @@ package com.meeweel.anilist.presentation.mainFragment.adapter.viewHolders
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.meeweel.anilist.R
 import com.meeweel.anilist.app.App
@@ -20,12 +21,18 @@ class NotWatchedViewHolder(
 ) : BaseViewHolder(binding.root) {
     override fun bind(anime: ShortAnime) {
         binding.apply {
+
+            val circularProgressDrawable = CircularProgressDrawable(this.notWatchedFragmentRecyclerItemImageView.context)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+
             notWatchedFragmentRecyclerItemTextView.text =
                 if (App.ContextHolder.context.resources.getBoolean(R.bool.isRussian)) anime.ruTitle else anime.enTitle
             Glide.with(this.notWatchedFragmentRecyclerItemImageView.context)
                 .load(anime.image)
-                .error(R.drawable.anig)
-                .placeholder(R.drawable.anig)
+                .error(circularProgressDrawable)
+                .placeholder(circularProgressDrawable)
                 .into(this.notWatchedFragmentRecyclerItemImageView)
 
             itemData.text = anime.data
